c49fe32b4a18e53c57d384f119e49ae3faa8ae68
Patrick Mihalcea <patrick.mihalcea@capco.com>
2025-04-08 14:51:26 -0400

    TopKFrequentElements

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
-
+import java.util.Arrays;
 
 public class Main {  
     public static void main(String[] args) {
-        int inputSize = 1;
-        boolean checkSolutions = true;
+        int inputSize = 2;
+        boolean checkSolutions = false;
         
         String[] TestCases = {
-            "[act, pots, tops, cat, stop, hat]",
-            "[]"
+            "[1,2,2,3,3,3]", "2",
+            "[7, 7]", "1"
         };
 
         String[] Solutions = {
@@ -18,11 +18,12 @@ public class Main {
         int testCaseNumber = 0;
         // Loop through test cases and call function here. 
         for (int i = 0; i < TestCases.length; i+=inputSize) {
-            String[] strs = InputHelper.argToStringArray(TestCases[i]);
+            // String[] strs = InputHelper.argToStringArray(TestCases[i]);
+            int[] nums = InputHelper.argToIntArray(TestCases[i]);
             System.out.print("Test Case " + Integer.toString(testCaseNumber+1) + ": "); 
 
             // Function Call!
-            String attempt = GroupAnagrams_49.Solution(strs).toString();
+            String attempt = Arrays.toString(TopKFrequentElements_347.Solution(nums, Integer.parseInt(TestCases[i+1])));
 
             if (checkSolutions) {
                 if (attempt.equals(Solutions[testCaseNumber])) {
--- TopKFrequentElements_347.java
+++ TopKFrequentElements_347.java
@@ -2,7 +2,23 @@ import java.util.*;
 
 public class TopKFrequentElements_347 {
     public static int[] Solution(int[] nums, int k) {
-        int[] solution = new int[2];
+        int[] solution = new int[k];
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.merge(num, 1, Integer::sum); // Increment frequency, adds if missing.
+        }
+
+        PriorityQueue<Map.Entry<Integer,Integer>> pq = 
+            new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+        
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            pq.offer(entry);
+        }
+
+        for (int i = 0; i < k; i++) {
+            solution[i] = pq.poll().getKey();
+        }
 
         return solution;
     }
