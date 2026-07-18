b974a31a96e5eea8e222e490380e2c42b814cdb8
PatrickMihalcea <64994754+PatrickMihalcea@users.noreply.github.com>
2026-07-18 14:41:56 -0400

    Solve 128. Longest Consecutive Sequence (java)

--- 128_Longest_Consecutive_Sequence/java/128_Longest_Consecutive_Sequence_2026-07-18.java
+++ 128_Longest_Consecutive_Sequence/java/128_Longest_Consecutive_Sequence_2026-07-18.java
@@ -1,37 +1,20 @@
-class Solution {
+public class Solution {
     public int longestConsecutive(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int longestConsecutive = 0;
+        Set<Integer> numSet = new HashSet<>();
         for (int num : nums) {
-            if (map.containsKey(num)) {
-                continue;
-            }
-            else if ((map.containsKey(num + 1)) && (map.containsKey(num - 1))) {
-                int low = map.get(num-1);
-                int high = map.get(num+1);
-                map.put(low, high);
-                map.put(high, low);
-                longestConsecutive = Math.max(longestConsecutive, high - low + 1);
-                map.put(num, -1);
-            }
-            else if (map.containsKey(num - 1)) {
-                int low = map.get(num-1);
-                map.put(num, low);
-                map.put(low, num);
-                longestConsecutive = Math.max(longestConsecutive, num - low + 1);
-            }
-            else if (map.containsKey(num + 1)) {
-                int high = map.get(num+1);
-                map.put(num, high);
-                map.put(high, num);
-                longestConsecutive = Math.max(longestConsecutive, high - num + 1);
-            }
-            else {
-                map.put(num, num);
-                longestConsecutive = Math.max(longestConsecutive, 1);
-            }      
+            numSet.add(num);
         }
+        int longest = 0;
 
-        return longestConsecutive;
+        for (int num : numSet) {
+            if (!numSet.contains(num - 1)) {
+                int length = 1;
+                while (numSet.contains(num + length)) {
+                    length++;
+                }
+                longest = Math.max(longest, length);
+            }
+        }
+        return longest;
     }
 }
