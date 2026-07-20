e7a68144f275ad74fdc08216454de640c5087661
PatrickMihalcea <64994754+PatrickMihalcea@users.noreply.github.com>
2026-07-20 12:32:16 -0400

    Solve 121. Best Time to Buy and Sell Stock (java)

--- 121_Best_Time_to_Buy_and_Sell_Stock/java/121_Best_Time_to_Buy_and_Sell_Stock_2026-07-20.java
+++ 121_Best_Time_to_Buy_and_Sell_Stock/java/121_Best_Time_to_Buy_and_Sell_Stock_2026-07-20.java
@@ -1,32 +1,17 @@
-class Solution {
+public class Solution {
     public int maxProfit(int[] prices) {
-        if (prices.length < 2) {
-            return 0;
+        int l = 0, r = 1;
+        int maxP = 0;
+
+        while (r < prices.length) {
+            if (prices[l] < prices[r]) {
+                int profit = prices[r] - prices[l];
+                maxP = Math.max(maxP, profit);
+            } else {
+                l = r;
+            }
+            r++;
         }
-
-        int[][] minMax = new int[prices.length][2];
-        int left = 0;
-        int right = prices.length - 1;
-        int low = 1000;
-        int high = 0;
-
-        int maxProfit = 0;
-
-        while (left < prices.length) {
-            low = Math.min(low, prices[left]);
-            high = Math.max(high, prices[right]);
-
-            minMax[left][0] = low;
-            minMax[right][1] = high;
-
-            left++;
-            right--;
-        }
-
-        for (int i = 0; i < prices.length; i++) {
-            maxProfit = Math.max(maxProfit, (minMax[i][1] - minMax[i][0]));
-        }
-
-        return maxProfit;
+        return maxP;
     }
 }
